459b7ccd073a33520d2326606ec111faff1a310a
Josue Tinoco <jetinoco98@hotmail.com>
2023-11-19 11:51:24 +0100

    u

--- Homework6/Josue/run.sh
+++ Homework6/Josue/run.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 # Compile the programs
-gcc father.c -o father -lrt
-gcc child.c -o child -lrt
+gcc father.c -o father
+gcc child.c -o child
 
 # Run the father program
 ./father
 
-# Clean up compiled files
-rm -f father child
-
-echo "Programs executed and cleanup complete."
+echo "Programs executed"
